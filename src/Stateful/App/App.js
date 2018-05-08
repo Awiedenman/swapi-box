b2c168e8c562ffb51a4b9b037dd26f2583f2c402
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-08 16:01:59 -0600

    Make fetch for film crawl and update cleaner into that file

--- src/Stateful/App/App.js
+++ src/Stateful/App/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { filmCrawl } from '../../ApiCalls/FilmApi';
 
 class App extends Component {
+constructor() {
+  super();
+  this.state = {
+    film : [],
+    people : [],
+    vehicles : [],
+    planets: []
+  }
+
+}
+
+async componentDidMount() {
+  await filmCrawl()
+  console.log( await filmCrawl())
+}
+
   render() {
+
     return (
       <div className="App">
-        <Header className="App-header" />
-        <CardContainer />
+        {/* <Header className="App-header" /> */}
+        {/* <CardContainer /> */}
         
       </div>
     );
