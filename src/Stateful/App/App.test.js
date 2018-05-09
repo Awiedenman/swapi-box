9eb469c2b32beff426f3d6d19f4cd87869d8de05
Austin.Wiedenman <awiedenman@gmail.com>
2018-05-09 15:16:17 -0600

    Add release date to cleaner

--- src/Stateful/App/App.test.js
+++ src/Stateful/App/App.test.js
@@ -1,9 +1,24 @@
+import { shallow, mount } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+
+      beforeEach(() => {
+        wrapper = shallow( < App /> )
+      })
+
+      it.skip('should call starWarsData with the correct paramenters', () => {
+        const mockStarWarsDAta = jest.fn();
+        const mockParams = [{
+            'A New Hope': "It is a period of civil war" },
+          { 'Attack of the Clones': "There is unrest in the Galactic" },
+          { 'The Phantom Menace': "Turmoil has engulfed the Galactic Republic" }
+        ]
+
+        wrapper.instance().mockStarWarsDAta().toaHaveBeenCalledWith(mockParams)
+
+      })
+
+    }
